aaabf33e58fbbfd57569d6f89d0a537b8c648c2e
Thomas O'Leary <t.oleary@me.com>
2022-09-15 16:28:10 -0700

    add breathe 1-5 emergencyGPS icons to good and bad

---
+++ screens/Five.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { Text, StyleSheet, View, TouchableOpacity } from "react-native";
+import { Entypo } from '@expo/vector-icons'; 
+
+const Five = ({ navigation }) => (
+  <View style={styles.container}>
+    <Text style={styles.add}>Name five things that you can see.</Text>
+    <TouchableOpacity onPress={() => navigation.navigate("Four")} delayPressIn={150}>
+      <Entypo style={styles.icon} name="arrow-with-circle-right" />
+    </TouchableOpacity>
+  </View>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor:"#1B2A41"
+  },
+  add: {
+    borderRadius: 10,
+    borderWidth: 4,
+    borderColor: "#D7D9D7",
+    marginTop: 21,
+    textAlign: "center",
+    padding: 10,
+    fontSize: 40,
+    fontWeight: "bold",
+    color: "#D7D9D7",
+  },
+  icon: {
+    paddingTop: 40,
+    fontSize: 50,
+    color: "#D7D9D7"
+  }
+
+});
+
+export { Five }
