7dde94b433a2a11039928fbfb70da3fc82372adc
Thomas O'Leary <t.oleary@me.com>
2022-09-22 16:48:28 -0700

    add Activities, Pies, PMR, and nav to all

---
+++ screens/ProgressiveMuscle.js
@@ -0,0 +1,29 @@
+const ProgressiveMuscle = () => (
+  <View style={styles.container}>
+  <Text style={styles.add}>Progressive Muscle Relaxation</Text>
+  </View>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 50,
+    backgroundColor:"#1B2A41"
+  },
+  add: {
+    borderRadius: 10,
+    borderWidth: 4,
+    borderColor: "#D7D9D7",
+    marginTop: 21,
+    width: "80%",
+    left: "10%",
+    textAlign: "center",
+    justifyContent: "flex-end",
+    padding: 10,
+    fontSize: 25,
+    fontWeight: "bold",
+    color: "#D7D9D7",
+  },
+});
+
+export { ProgressiveMuscle };
