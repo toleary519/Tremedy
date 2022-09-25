9dc1ef6e63af3f7e9953bb8b39047cfd0426571d
Thomas O'Leary <t.oleary@me.com>
2022-09-24 21:32:54 -0700

    added PMR and Stoic instruct, finish activities

---
+++ screens/PMRInstruct.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Text, StyleSheet, View } from "react-native";
+import { FontAwesome5 } from '@expo/vector-icons';
+
+const PMRInstruct = () => {
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.add}>PMR Instructions</Text>
+    </View>
+  )
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#1B2A41"
+  },
+  add: {
+    textAlign: "center",
+    justifyContent: "flex-end",
+    padding: 15,
+    fontSize: 30,
+    fontWeight: "bold",
+    color: "#D7D9D7",
+  },
+});
+
+export { PMRInstruct };
