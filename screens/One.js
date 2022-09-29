ae272ed8b2c10b8d7b1ea9693d73ac3ac2d425b5
Thomas O'Leary <t.oleary@me.com>
2022-09-29 08:14:31 -0700

    add stoic Qs and Focus Statement, add delete to input maps

--- screens/One.js
+++ screens/One.js
@@ -20,7 +20,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
-    alignItems: "center",
     backgroundColor:"#1B2A41"
   },
   add: {
@@ -30,6 +29,8 @@ const styles = StyleSheet.create({
     marginTop: 21,
     textAlign: "center",
     padding: 10,
+    width: "90%",
+    left: "5%",
     fontSize: 40,
     fontWeight: "bold",
     color: "#D7D9D7",
@@ -37,6 +38,7 @@ const styles = StyleSheet.create({
   icon: {
     paddingTop: 40,
     fontSize: 50,
+    left: "43%",
     color: "#D7D9D7"
   }
 
