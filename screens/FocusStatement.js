4572fa4095d7cbbbf3d6e38bbab6d38c6d953329
Thomas O'Leary <t.oleary@me.com>
2022-09-29 08:15:58 -0700

    add stoic Qs and Focus Statement, add delete to input maps

---
+++ screens/FocusStatement.js
@@ -0,0 +1,172 @@
+import React, { useState } from "react";
+import { Text, StyleSheet, View, ScrollView, TextInput, TouchableOpacity, KeyboardAvoidingView } from "react-native";
+import { FontAwesome5 } from '@expo/vector-icons';
+import { MaterialIcons } from '@expo/vector-icons'; 
+import { A } from '@expo/html-elements';
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+
+const FocusStatement = () => {
+   
+  let fakeDB = []
+  
+  const [showDB, setShowDB] = useState(fakeDB)
+  const [myFocus, setMyFocus] = useState() 
+ 
+  const handleAdd = () => {
+        
+        let newFocus = {
+          id: myFocus,
+          myFocus: myFocus,
+        };
+
+        const newList = [...showDB, newFocus]
+        
+        setShowDB(newList);
+        setMyFocus("");
+    }
+
+    let currentDate = new Date();
+    let currentDay = currentDate.getDate();
+    let currentMonth = currentDate.getMonth() + 1;
+    let currentYear = currentDate.getFullYear();
+    let time = `${currentDate.getHours()}:${currentDate.getMinutes()}`
+
+  const handleDelete = ({ item }, index) => {
+          console.log("before: ", showDB)
+          setShowDB(showDB.filter((val) => val.id !== item.id));
+          showDB.splice(index, 1)
+          console.log("after: ", showDB)
+  }
+
+
+  return (
+    <KeyboardAwareScrollView style={styles.container}>
+      <Text style={styles.header}>
+        Think of your focus. Who you want to be and who you are.
+      </Text>
+      <Text style={styles.headerTwo}>
+        Enter it below. As you grow update it if you feel the need and when in doubt refer to it as a guiding principle.
+      </Text>
+      <TextInput 
+        style={styles.input} 
+        onChangeText={(text) => setMyFocus(text)}
+        value={myFocus}
+        placeholder={"focus statement"} 
+        multiline
+        keyboardType="default"
+        color="#D7D9D7"
+        placeholderTextColor={"#F1F7EE"}    
+      />
+      <TouchableOpacity onPress={() => handleAdd()}>
+        <MaterialIcons style={styles.icon} name="add-circle" />
+      </TouchableOpacity>
+      <View>
+        {showDB.map((item) => (
+          <View key={item.id} style={styles.pieContainer}>
+            <Text style={styles.date}>{currentMonth}/{currentDay}/{currentYear}  {time}</Text>
+            <Text style={styles.add}>{item.myFocus}</Text>
+            <TouchableOpacity onPress={() => handleDelete({ item })}>
+              <MaterialIcons style={styles.deleteIcon} name="delete-forever"/>
+            </TouchableOpacity>
+          </View>
+        ))}
+      </View>
+    </KeyboardAwareScrollView>
+  )
+};
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor:"#1B2A41",
+    paddingBottom: 30
+  },
+  pieContainer: {
+    borderRadius: 10,
+    borderWidth: 4,
+    marginTop: 7,
+    marginBottom: 7,
+    width: "95%",
+    left: "2.5%",
+    borderColor: "#D7D9D7",
+  },
+  elementContainer: {
+    flexDirection: "row",
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  add: {
+    marginTop: 5,
+    width: "90%",
+    left: "5%",
+    textAlign: "flex-start",
+    alignItems: "center",
+    marginBottom: 5,
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "#D7D9D7",
+  },
+  date: {
+    marginTop: 5,
+    width: "90%",
+    left: "5%",
+    textAlign: "center",
+    justifyContent: "flex-end",
+    padding: 10,
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "#D7D9D7",
+  },
+  header: {
+    paddingTop: 30,
+    textAlign: "center",
+    justifyContent: "flex-start",
+    alignItems: "flex-start",
+    padding: 10,
+    fontSize: 25,
+    fontWeight: "bold",
+    color: "#D7D9D7",
+  },
+  headerTwo: {
+    // borderRadius: 10,
+    // borderWidth: 4,
+    paddingTop: 30,
+    textAlign: "center",
+    justifyContent: "flex-start",
+    alignItems: "flex-start",
+    padding: 10,
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "#D7D9D7",
+  },
+  icon: {
+    paddingTop: 20,
+    paddingBottom: 20,
+    fontSize: 30,
+    color: "#D7D9D7",
+    textAlign: "center",
+  },
+  input: {
+    borderRadius: 10,
+    borderWidth: 4,
+    borderColor: "#D7D9D7",
+    width: "80%",
+    marginTop: 21,
+    textAlign: "center",
+    justifyContent: "center",  
+    padding: 10,
+    left: "10%",
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "#2f8587",
+  },
+  deleteIcon: {
+    paddingTop: 20,
+    paddingBottom: 20,
+    left: "45%",
+    fontSize: 30,
+    color: "#D7D9D7",
+  },
+});
+
+export { FocusStatement };
+
