6e9cccb133bdcfed0005c6b8bbe96fcde782630a
Thomas O <t.oleary@me.com>
2022-10-28 21:08:57 -0600

    peptalk routine add fixed, routineup down working

--- screens/Routine.js
+++ screens/Routine.js
@@ -1,92 +1,108 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Text,
   StyleSheet,
   View,
+  Alert,
+  TextInput,
   TouchableOpacity,
   ScrollView,
 } from "react-native";
-import { FontAwesome5 } from "@expo/vector-icons";
-// import { NotesMenu } from "./NotesMenu";
-// import { SoberContacts } from "./SoberContacts";
-// import { BadTimes } from "./BadTimes";
-// import ProConPage from "./ProConPage";
-// import { GoodTimes } from "./GoodTimes";
-// import { PercievedThreatMenu } from "./PercievedThreatMenu";
-// import { Activities } from "./Activities";
-// import { Pies } from "./Pies";
-// import { SelfTalk } from "./SelfTalk";
-// import { MyValues } from "./MyValues";
-// import { FocusStatement } from "./FocusStatement";
-// import { CopingStatement } from "./CopingStatement";
-// import { CheckIn } from "./CheckIn";
+import { Feather } from "@expo/vector-icons";
+import { SimpleLineIcons } from "@expo/vector-icons";
+import { MaterialIcons } from "@expo/vector-icons";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const Routine = ({ navigation }) => {
-  pepData;
+  let [selectedRoutine, setSelectedRoutine] = useState(
+    selectedRoutine ? selectedRoutine : []
+  );
 
-  arr[i];
-
-  let availPages = [
-    {
-      id: 1,
-      selected: true,
-      pageName: "PercievedThreatMenu",
-      title: "Elevated State",
-    },
-    { id: 2, selected: true, pageName: "Report", title: "My Past Week" },
-    {
-      id: 3,
-      selected: true,
-      pageName: "FocusStatement",
-      title: "My Focus Statement",
-    },
-    {
-      id: 4,
-      selected: true,
-      pageName: "CopingStatement",
-      title: "My Coping Statement",
-    },
-    { id: 5, selected: true, pageName: "MyValues", title: "My Values" },
-    { id: 6, selected: false, pageName: "CheckIn", title: "Quick Check-In" },
-    { id: 7, selected: false, pageName: "SelfTalk", title: "+ Self-Talk" },
-    { id: 8, selected: false, pageName: "Pies", title: "PIES Check-In" },
-    { id: 9, selected: false, pageName: "Five", title: "5-4-3-2-1" },
-    { id: 10, selected: true, pageName: "NotesMenu", title: "Experiences" },
-    {
-      id: 11,
-      selected: true,
-      pageName: "SoberContacts",
-      title: "Support Contacts",
-    },
-    { id: 12, selected: true, pageName: "ProCon", title: "Pros & Cons" },
-    { id: 13, selected: true, pageName: "Activities", title: "Activities" },
-  ];
-
-  console.log(availPages);
-  //   selectedRoutine = [];
-  let selectedRoutine = availPages.filter((x) => x.selected);
-
-  const buildRoutine = () => {
-    for (let page of availPages) {
-      if (page.selected === true) {
-        selectedRoutine.push(page);
-      }
+  const getData = async () => {
+    try {
+      const jsonValue = await AsyncStorage.getItem("savedRoutine");
+      let savedData = jsonValue ? JSON.parse(jsonValue) : [];
+      setSelectedRoutine(savedData);
+    } catch (e) {
+      console.log(e);
     }
   };
 
-  React.useEffect(() => buildRoutine(), []);
+  const storeData = async (XY) => {
+    try {
+      const jsonValue = JSON.stringify(XY);
+      await AsyncStorage.setItem("savedRoutine", jsonValue);
+    } catch (e) {
+      console.log("1", e);
+    }
+  };
+
+  let endCheck = () => {
+    Alert.alert(`Whoops!`, `You're already at the end of the list.`, [
+      {
+        text: "OK",
+        style: "cancel",
+        onPress: () => {
+          return;
+        },
+      },
+    ]);
+  };
+
+  const swapUp = (i) => {
+    var temp = selectedRoutine[i];
+
+    selectedRoutine[i] = selectedRoutine[i - 1];
+    selectedRoutine[i - 1] = temp;
+
+    storeData(selectedRoutine);
+    getData();
+  };
+
+  const swapDown = (i) => {
+    var temp = selectedRoutine[i];
+
+    selectedRoutine[i] = selectedRoutine[i + 1];
+    selectedRoutine[i + 1] = temp;
+
+    storeData(selectedRoutine);
+    getData();
+  };
+
+  const end = selectedRoutine.length - 1;
+
+  React.useEffect(() => {
+    getData();
+  }, []);
 
   return (
     <View style={styles.container}>
       <ScrollView contentContainerStyle={{ paddingBottom: 30 }}>
         {selectedRoutine
-          ? selectedRoutine.map((item) => (
-              <View key={item.id}>
+          ? selectedRoutine.map((item, i) => (
+              <View key={item.id} style={styles.element}>
                 <TouchableOpacity
-                  keyonPress={() => navigation.navigate(item.pageName)}
+                  onPress={i === end ? () => endCheck() : () => swapDown(i)}
+                  style={styles.left}
                   delayPressIn={150}
                 >
-                  <Text style={styles.add}>{item.title}</Text>
+                  <Feather name="chevrons-down" style={styles.arrow} />
+                </TouchableOpacity>
+                <View style={styles.middle}>
+                  <TouchableOpacity
+                    onPress={() => navigation.navigate(item.pageName)}
+                    delayPressIn={150}
+                  >
+                    <Text style={styles.add}>{item.title}</Text>
+                  </TouchableOpacity>
+                </View>
+                <TouchableOpacity
+                  onPress={i === 0 ? () => endCheck() : () => swapUp(i)}
+                  style={styles.right}
+                  delayPressIn={150}
+                >
+                  <Feather name="chevrons-up" style={styles.arrow} />
                 </TouchableOpacity>
               </View>
             ))
@@ -104,8 +120,11 @@ const styles = StyleSheet.create({
     paddingTop: 15,
     backgroundColor: "#1B2A41",
   },
-  add: {
+  element: {
     overflow: "hidden",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
     borderRadius: 10,
     borderWidth: 4,
     borderColor: "#F4743B",
@@ -113,12 +132,35 @@ const styles = StyleSheet.create({
     marginTop: 21,
     width: "90%",
     left: "5%",
-    textAlign: "center",
-    justifyContent: "flex-end",
-    padding: 10,
-    fontSize: 25,
-    fontWeight: "bold",
+  },
+  add: {
     color: "#1B2A41",
+    fontSize: 25,
+    padding: 10,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  arrow: {
+    color: "#1B2A41",
+    fontSize: 24,
+  },
+  left: {
+    flex: 1,
+    // justifyContent: "center",
+    alignItems: "center",
+    // borderWidth: 2,
+    // borderColor: "black",
+  },
+  middle: {
+    flex: 8,
+    // borderWidth: 2,
+    // borderColor: "black",
+  },
+  right: {
+    flex: 1,
+    alignItems: "center",
+    // borderWidth: 2,
+    // borderColor: "black",
   },
 });
 
