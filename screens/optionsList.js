9325ee155159752f5bd09fdcae10f34e207404d0
Thomas O <t.oleary@me.com>
2022-10-28 21:09:28 -0600

    peptalk routine add fixed, routineup down working

--- screens/optionsList.js
+++ screens/optionsList.js
@@ -1,37 +1,36 @@
-let pageOptions = [
+export const pageOptions = [
   {
     id: 1,
-    selected: true,
+    selected: false,
     pageName: "PercievedThreatMenu",
     title: "Elevated State",
   },
-  { id: 2, selected: true, pageName: "Report", title: "My Past Week" },
-  {
-    id: 3,
-    selected: true,
-    pageName: "FocusStatement",
-    title: "My Focus Statement",
-  },
+  { id: 2, selected: false, pageName: "Report", title: "My Past Week" },
+  { id: 3, selected: false, pageName: "Routine", title: "My Routine" },
   {
     id: 4,
-    selected: true,
+    selected: false,
     pageName: "CopingStatement",
     title: "My Coping Statement",
   },
-  { id: 5, selected: true, pageName: "MyValues", title: "My Values" },
-  { id: 6, selected: false, pageName: "CheckIn", title: "Quick Check-In" },
+  { id: 5, selected: false, pageName: "MyValues", title: "My Values" },
+  { id: 6, selected: true, pageName: "CheckIn", title: "Quick Check-In" },
   { id: 7, selected: false, pageName: "SelfTalk", title: "+ Self-Talk" },
   { id: 8, selected: false, pageName: "Pies", title: "PIES Check-In" },
-  { id: 9, selected: false, pageName: "Five", title: "5-4-3-2-1" },
-  { id: 10, selected: true, pageName: "NotesMenu", title: "Experiences" },
+  {
+    id: 9,
+    selected: false,
+    pageName: "FocusStatement",
+    title: "My Focus Statement",
+  },
+  { id: 10, selected: false, pageName: "NotesMenu", title: "Experiences" },
   {
     id: 11,
-    selected: true,
+    selected: false,
     pageName: "SoberContacts",
     title: "Support Contacts",
   },
-  { id: 12, selected: true, pageName: "ProCon", title: "Pros & Cons" },
-  { id: 13, selected: true, pageName: "Activities", title: "Activities" },
+  { id: 12, selected: false, pageName: "ProCon", title: "Pros & Cons" },
+  { id: 13, selected: false, pageName: "Activities", title: "Activities" },
+  { id: 14, selected: false, pageName: "Five", title: "5-4-3-2-1" },
 ];
-
-export default pageOptions;
