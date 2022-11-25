d57ac69d176d272541760bf67ef0b82335310e98
Thomas O <t.oleary@me.com>
2022-11-25 10:06:18 -0700

    fix scream on load and personal best counter

--- screens/Scream.js
+++ screens/Scream.js
@@ -5,9 +5,12 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import { look } from "../assets/styles";
 
 const Scream = () => {
-  const [best, setBest] = useState(best ? best : 0);
+  const [best, setBest] = useState(best ? best : []);
   const [time, setTime] = useState(0);
   const [running, setRunning] = useState(false);
+  let a = ("0" + Math.floor((time / 60000) % 60)).slice(-2);
+  let b = ("0" + Math.floor((time / 1000) % 60)).slice(-2);
+  let c = ("0" + ((time / 10) % 100)).slice(-2);
 
   useEffect(() => {
     let interval;
@@ -30,7 +33,7 @@ const Scream = () => {
       console.log(e);
     }
   };
-
+  // console.log("best : ", best);
   const storeData = async (best) => {
     try {
       const jsonValue = JSON.stringify(best);
@@ -41,13 +44,17 @@ const Scream = () => {
   };
 
   const highScore = () => {
-    let a = ("0" + Math.floor((time / 60000) % 60)).slice(-2);
-    let b = ("0" + Math.floor((time / 1000) % 60)).slice(-2);
-    let c = ("0" + ((time / 10) % 100)).slice(-2);
-    let bestTotal = best ? best[0] + best[1] + best[2] : 0;
-    if (a + b + c > bestTotal) {
-      setBest([a, b, c]);
+    let bestTotal =
+      best.length > 0
+        ? parseInt(best[0], 10) +
+          parseInt(best[1], 10) * 100 +
+          parseInt(best[2], 10)
+        : 0;
+
+    if (parseInt(a, 10) + parseInt(b, 10) * 100 + parseInt(c, 10) > bestTotal) {
       let newBest = [a, b, c];
+      setBest(newBest);
+      console.log("best after set : ", best);
       storeData(newBest);
       getData();
     }
@@ -65,16 +72,18 @@ const Scream = () => {
         <View>
           <View style={[look.border, { paddingBottom: "3%" }]}>
             <Text style={look.header}>
-              Some times you just have to scream like a banshee at the moon.
+              Sometimes you just have to scream like a banshee at the moon.
             </Text>
             <Text style={look.sub}>
               No audio is saved. Shaking the phone is encouraged.
             </Text>
           </View>
           <View style={look.elementHeader}>
-            <Text style={[look.sub]}>Personal Best :</Text>
-            <Text style={look.sub}>
-              {best ? `  ${best[0]}:${best[1]}:${best[2]}` : `  00:00:00`}
+            <Text style={[look.sub, { fontSize: 15 }]}>Personal Best :</Text>
+            <Text style={[look.sub, { fontSize: 15 }]}>
+              {best.length > 0
+                ? `  ${best[0]}:${best[1]}:${best[2]}`
+                : `  00:00:00`}
             </Text>
           </View>
           <View style={look.clock}>
@@ -113,7 +122,12 @@ const Scream = () => {
                 getData();
               }}
             >
-              <Text style={[look.sub, { marginTop: "10%", opacity: 0.8 }]}>
+              <Text
+                style={[
+                  look.sub,
+                  { marginTop: "10%", opacity: 0.8, fontSize: 15 },
+                ]}
+              >
                 Reset Personal Best
               </Text>
             </TouchableOpacity>
@@ -122,6 +136,6 @@ const Scream = () => {
       </View>
     </View>
   );
-};
+};;;;;
 
 export { Scream };
