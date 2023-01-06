638c234b6f3a1177cc47227e62ef9f015a88a510
Thomas O <t.oleary@me.com>
2023-01-05 17:08:50 -0700

    add contact to toolbox

--- screens/PeptalkMenu.js
+++ screens/PeptalkMenu.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   Text,
   StyleSheet,
@@ -9,10 +9,12 @@ import {
 import { Feather } from "@expo/vector-icons";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { pageOptions } from "../helpers/optionsList";
+import * as MailComposer from "expo-mail-composer";
 import { look } from "../assets/styles";
 
 const PepTalkMenu = ({ navigation }) => {
   let [routineList, setRoutineList] = useState(routineList ? routineList : []);
+  const [isAvailable, setIsAvailable] = useState(false);
 
   const getData = async () => {
     try {
@@ -67,6 +69,15 @@ const PepTalkMenu = ({ navigation }) => {
     storeData(routineList);
   };
 
+  const sendContactEmail = () => {
+    MailComposer.composeAsync({
+      subject: `Notice : Ourtre Team`,
+      body: "The floor is yours.",
+      recipients: "contact@ourtre.com",
+      attachments: [],
+    });
+  };
+
   const verify = ({ item }) => {
     for (let x of routineList) {
       if (x.id === item.id) {
@@ -77,6 +88,14 @@ const PepTalkMenu = ({ navigation }) => {
   };
 
   const count = routineList.length ? routineList.length : 0;
+
+  useEffect(() => {
+    async function checkAvailability() {
+      const isMailAvailable = await MailComposer.isAvailableAsync();
+      setIsAvailable(isMailAvailable);
+    }
+    checkAvailability();
+  }, []);
 
   React.useEffect(() => {
     getData();
@@ -123,6 +142,19 @@ const PepTalkMenu = ({ navigation }) => {
               </View>
             </View>
           ))}
+          <View style={look.border}>
+            <View style={look.element}>
+              <View style={look.left}>
+                <TouchableOpacity
+                  delayPressIn={150}
+                  onPress={() => sendContactEmail()}
+                >
+                  <Text style={look.add}>Contact Us</Text>
+                  <Text style={look.sub}>We're all ears.</Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+          </View>
         </View>
       </ScrollView>
     </View>
