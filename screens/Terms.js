abbfc906b1dcaf98de28e8a7408588151ccde0e8
Thomas O <t.oleary@me.com>
2023-01-14 00:47:05 -0700

    update terms

--- screens/Terms.js
+++ screens/Terms.js
@@ -16,30 +16,25 @@ const Terms = () => {
               information, tools and Services available from this site to you,
               the user, conditioned upon your acceptance of all terms,
               conditions, policies and notices stated here. By visiting our site
-              and/ or purchasing something from us, you engage in our “Service”
-              and agree to be bound by the following terms and conditions
-              (“Terms of Service”, “Terms”), including those additional terms
-              and conditions and policies referenced herein and/or available by
-              hyperlink. These Terms of Service apply to all users of the site,
-              including without limitation users who are browsers, vendors,
-              customers, merchants, and/ or contributors of content. Please read
-              these Terms of Service carefully before accessing or using our
-              App. By accessing or using any part of the site, you agree to be
-              bound by these Terms of Service. If you do not agree to all the
-              terms and conditions of this agreement, then you may not access
-              the App or use any Services. If these Terms of Service are
-              considered an offer, acceptance is expressly limited to these
-              Terms of Service. Any new features or tools which are added to the
-              current store shall also be subject to the Terms of Service. You
-              can review the most current version of the Terms of Service at any
-              time on this page. We reserve the right to update, change or
-              replace any part of these Terms of Service by posting updates
-              and/or changes to our App. It is your responsibility to check this
-              page periodically for changes. Your continued use of or access to
-              the App following the posting of any changes constitutes
-              acceptance of those changes. Our store is hosted on Shopify Inc.
-              They provide us with the online e-commerce platform that allows us
-              to sell our products and Services to you.
+              you engage in our “Service” and agree to be bound by the following
+              terms and conditions (“Terms of Service”, “Terms”). These Terms of
+              Service apply to all users of the App, including without
+              limitation users who are browsers, vendors, customers, merchants,
+              and/ or contributors of content. Please read these Terms of
+              Service carefully before accessing or using our App. By accessing
+              or using any part of the site, you agree to be bound by these
+              Terms of Service. If you do not agree to all the terms and
+              conditions of this agreement, then you may not access the App or
+              use any Services. If these Terms of Service are considered an
+              offer, acceptance is expressly limited to these Terms of Service.
+              Any new features or tools which are added to the application shall
+              also be subject to the Terms of Service. You can review the most
+              current version of the Terms of Service at any time on this page.
+              We reserve the right to update, change or replace any part of
+              these Terms of Service by posting updates and/or changes to our
+              App. It is your responsibility to check this page periodically for
+              changes. Your continued use of or access to the App following the
+              posting of any changes constitutes acceptance of those changes.
             </Text>
           </View>
           <View style={{ marginBottom: "2%" }}>
@@ -106,46 +101,24 @@ const Terms = () => {
             <Text style={{ color: "#D7DAE5" }}>
               SECTION 5 - PRODUCTS OR SERVICES (if applicable) Certain products
               or Services may be available exclusively online through the App.
-              These products or Services may have limited quantities and are
-              subject to return or exchange only according to our Return Policy.
-              To view our Return Policy, please visit [LINK TO RETURN POLICY] We
-              have made every effort to display as accurately as possible the
-              colors and images of our products that appear at the store. We
-              cannot guarantee that your computer monitor's display of any color
-              will be accurate. We reserve the right, but are not obligated, to
-              limit the sales of our products or Services to any person,
-              geographic region or jurisdiction. We may exercise this right on a
-              case-by-case basis. We reserve the right to limit the quantities
-              of any products or Services that we offer. All descriptions of
-              products or product pricing are subject to change at anytime
-              without notice, at the sole discretion of us. We reserve the right
-              to discontinue any product at any time. Any offer for any product
-              or service made on this site is void where prohibited. We do not
-              warrant that the quality of any products, Services, information,
-              or other material purchased or obtained by you will meet your
-              expectations, or that any errors in the Service will be corrected.
+              We reserve the right, but are not obligated, to limit the sales of
+              our products or Services to any person, geographic region or
+              jurisdiction. We may exercise this right on a case-by-case basis.
+              We reserve the right to limit the quantities of any products or
+              Services that we offer. All descriptions of products or product
+              pricing are subject to change at anytime without notice, at the
+              sole discretion of us. We reserve the right to discontinue any
+              product at any time. Any offer for any product or service made on
+              this site is void where prohibited. We do not warrant that the
+              quality of any products, Services, information, or other material
+              purchased or obtained by you will meet your expectations, or that
+              any errors in the Service will be corrected.
             </Text>
           </View>
           <View style={{ marginBottom: "2%" }}>
             <Text style={{ color: "#D7DAE5" }}>
               SECTION 6 - ACCURACY OF BILLING AND ACCOUNT INFORMATION We reserve
-              the right to refuse any order you place with us. We may, in our
-              sole discretion, limit or cancel quantities purchased per person,
-              per household or per order. These restrictions may include orders
-              placed by or under the same customer account, the same credit
-              card, and/or orders that use the same billing and/or shipping
-              address. In the event that we make a change to or cancel an order,
-              we may attempt to notify you by contacting the e-mail and/or
-              billing address/phone number provided at the time the order was
-              made. We reserve the right to limit or prohibit orders that, in
-              our sole judgment, appear to be placed by dealers, resellers or
-              distributors. You agree to provide current, complete and accurate
-              purchase and account information for all purchases made at our
-              store. You agree to promptly update your account and other
-              information, including your email address and credit card numbers
-              and expiration dates, so that we can complete your transactions
-              and contact you as needed. For more detail, please review our
-              Returns Policy.
+              the right to refuse any order you place with us.
             </Text>
           </View>
           <View style={{ marginBottom: "2%" }}>
